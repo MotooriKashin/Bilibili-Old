607fcc567a393251a86cc5178282681495dbdd2f
201411232004 <60416767+201411232004@users.noreply.github.com>
2020-01-29 14:25:10 +0800

    Add files via upload

---
+++ bilibili/js-css/vipPlugin.js
@@ -0,0 +1,59 @@
+window.vipPopup = function(options,callback){
+    window.onmessage = function(e) {
+        e = e || window.event;
+        switch(e.data) {
+            case 'closeVipPlugin':
+                $('#biliVipPlugin').remove();
+                uploadData(function(){  //埋点
+                    rec_rp("event", "vip_web_pay_failure", {
+                        reason: '1',
+                        optype:options.type==1 && 2 || 1,
+                        from:options.fromid || '',
+                        title:options.fromtitle || ''
+                    });
+                });
+            break;
+            case 'vipGoLogin':
+                window.location.href = 'https://account.bilibili.com/login?gourl=' + encodeURIComponent(location.href);
+            break;
+            case 'vipPaySuccess' :
+                callback();
+                $('#biliVipPlugin').remove();
+            break;
+        }
+    };
+    var param = (options && options.type==1) ? '?type=1' : '?type=2'
+    if(!!options.fromid) param += '&fromid=' + options.fromid;
+    if(!!options.fromtitle) param += '&fromtitle=' + options.fromtitle;
+    if(!!options.appid) param += '&appid=' + options.appid;
+    if(!!options.subid) param += '&subid=' + options.subid;
+    var iframe = $('<iframe name="biliVipPlugin" id="biliVipPlugin" src="//vip.bilibili.com/site/vip-payplugin.html'+param+'" allowTransparency="true" frameborder="0" style="position:fixed;top:0;left:0;width:100%;height:100%;background-color:transparent;filter: progid:DXImageTransform.Microsoft.Alpha(opacity=0);z-index:99999999999999999999;"></iframe>').appendTo($('body')).show();
+};
+
+window.vipDisable = function(){
+    window.onmessage = function(e) {
+        e = e || window.event;
+        switch(e.data) {
+            case 'closeVipDisable': $('#biliVipDisable').remove();
+            break;
+        }
+    };
+    var iframe = $('<iframe name="biliVipDisable" id="biliVipDisable" src="//vip.bilibili.com/site/vip-disable.html" allowTransparency="true" frameborder="0" style="position:fixed;top:0;left:0;width:100%;height:100%;background-color:transparent;filter: progid:DXImageTransform.Microsoft.Alpha(opacity=0);z-index:99999999999999999999;"></iframe>').appendTo($('body')).show();
+};
+
+
+//数据埋点
+var dataReady;
+$.getScript('//data.bilibili.com/rec.js', function(ret) {
+    dataReady = true;
+});
+var uploadData = function(fn){
+    if(!fn) return;
+    if(dataReady){
+        fn();
+    }else{
+        setTimeout(function(){
+            uploadData(fn);
+        },1000)
+    }
+}
