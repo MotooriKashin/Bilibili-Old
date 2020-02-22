32431218a76ec681d83905f9cb63709e1e70b727
MotooriKashin <60416767+MotooriKashin@users.noreply.github.com>
2020-02-22 11:18:27 +0800

    修复版头bug
    ①修复mini版头和完整版头反了的bug
    ②匹配全网以解决嵌入式播放器跨域问题

--- main.user.js
+++ main.user.js
@@ -1,13 +1,12 @@
 // ==UserScript==
 // @name         Bilibili 旧播放页
 // @namespace    Motoori Kashin
-// @version      2.5.3
-// @description  切换Bilibili旧版HTML5播放器，恢复2019年12月09日之前的界面。已实现video/bangumi/watchlater/mylist及部分blackboard页面。若出现一般的载入异常请先尝试刷新解决。
+// @version      2.5.4
+// @description  切换Bilibili旧版HTML5播放器，恢复2019年12月09日之前的界面。已实现video/bangumi/watchlater/mylist及嵌入式播放器。
 // @author       Motoori Kashin
 // @homepageURL  https://github.com/MotooriKashin/Bilibili-Old/
 // @supportURL   https://github.com/MotooriKashin/Bilibili-Old/issues
-// @match        *://*.bilibili.com/*
-// @match        *://*.biligame.com/*
+// @match        *://*/*
 // @license      MIT License
 // @run-at       document-start
 // @icon         https://static.hdslb.com/images/favicon.ico
@@ -17,6 +16,10 @@
 
 (function() {
     'use strict';
+
+    /* 设置为全网脚本是处理bilibili嵌入播放器的跨域权限 */
+    /* 若有强迫症，可自行将上面@match后面替换为【*://*.bilibili.com/*】 */
+    /* 那将导致非bilibili域名下的嵌入播放器替换失败 */
 
     let INITIAL_DOCUMENT = ""; // 网页源代码，为避免多余请求，需要时再通过url获取(尚不知如何直接从本地获取)
     let INITIAL_TITLE = document.getElementsByTagName("title");if (INITIAL_TITLE[0]){INITIAL_TITLE = INITIAL_TITLE[0].innerText;} // 网页原标题
@@ -182,7 +185,7 @@
             let inf = document.getElementsByClassName("international-footer");
             if (inh){ // 判断版头类型
                 let ppt = document.getElementById("primaryPageTab");
-                if (ppt){functionInterface.setMiniHead(inh);}else{functionInterface.setTotalHead(inh);}
+                if (ppt){functionInterface.setTotalHead(inh);}else{functionInterface.setMiniHead(inh);}
             }
             if (inf[0]){functionInterface.setFoot(inf[0]);} // 判断版底
         });},
@@ -251,14 +254,17 @@
             functionInterface.rewritePage(html);
             },
         "blackboard" : ()=>{
-            //document.addEventListener("DOMNodeInserted",() => {
-                let player = document.getElementsByTagName("iframe");
-                if (player[0]){
-                    for (let i = 0;i < player.length;i++){
-                        let src = player[i].src;
-                        if (src && src.match(/newplayer/)){
-                            let aid = 1 * src.match(/aid=[0-9]*/)[0].replace(/aid=/,"");let element = player[i];
-                            let width = player[i].offsetWidth;let height = player[i].offsetHeight;
+            let player = document.getElementsByTagName("iframe");
+            if (player[0]){
+                for (let i = 0;i < player.length;i++){
+                    let src = player[i].src;
+                    console.log(player[i]);
+                    if (src && src.match(/newplayer/)){
+                        let aid = 1 * src.match(/aid=[0-9]*/)[0].replace(/aid=/,"");
+                        let cid = src.match(/cid=[0-9]*/);if(cid && cid[0]){cid = 1 * cid[0].replace(/cid=/,"");}
+                        let element = player[i];
+                        let width = player[i].offsetWidth;let height = player[i].offsetHeight;
+                        if(!cid){ // 尝试用URL获取cid(可能会跨域注意！)
                             let xhr = new XMLHttpRequest();
                             let url = '//api.bilibili.com/x/player/pagelist?aid=' + aid + '&jsonp=jsonp';
                             xhr.open('GET',url,true);
@@ -271,25 +277,33 @@
                             }
                             xhr.send();
                         }
+                        else{
+                            let iframe = document.createElement("iframe");
+                            iframe.setAttribute("src",iframeplayer.blackboard(aid,cid));
+                            iframe.setAttribute("style",'width: '+ width +'px;height: ' + height + 'px;');
+                            element.replaceWith(iframe);
+                        }
                     }
                 }
-            //});
+            }
             },
         "home" : ()=>{
             let html = page.home;
             functionInterface.rewritePage(html);
             },
     }
-    if(INITIAL_PATH[3]){ // INITIAL_PATH[3]域名之后的第一个关键词
-        if(INITIAL_PATH[3] == 'video' && INITIAL_PATH[4].startsWith('av')){rewritePage.av();} // 普通av页
-        if(INITIAL_PATH[3] == 'watchlater'){rewritePage.watchlater();} // 稍后再看
-        if(INITIAL_PATH[3] == 'bangumi' && INITIAL_PATH[4] == 'play'){rewritePage.bangumi();} // bangumi(包括特殊页面)
-        if(INITIAL_PATH[3] == 'blackboard' && INITIAL_PATH[4] && INITIAL_PATH[4].startsWith('newplayer')){window.parent.postMessage("newplayer", "*")} // 嵌入式页面
-        if(INITIAL_PATH[2] != 'live.bilibili.com'){global.rewriteSction();} // 不处理直播页版面
-    }
-    if(INITIAL_PATH[2].match(/biligame/) == null && INITIAL_PATH[2].match(/live/) == null){
-        document.addEventListener("DOMContentLoaded",global.resetSction()); // 不对直播页和游戏页进行其他全局处理
+    if(INITIAL_PATH[2] && INITIAL_PATH[2].match(/bilibili\./)){ // 匹配*.bilibili.com
+        if(INITIAL_PATH[3]){
+            if(INITIAL_PATH[3] == 'video' && INITIAL_PATH[4].startsWith('av')){rewritePage.av();} // 普通av页
+            if(INITIAL_PATH[3] == 'watchlater'){rewritePage.watchlater();} // 稍后再看
+            if(INITIAL_PATH[3] == 'bangumi' && INITIAL_PATH[4] == 'play'){rewritePage.bangumi();} // bangumi(包括特殊页面)
+            if(INITIAL_PATH[3] == 'blackboard' && INITIAL_PATH[4] && INITIAL_PATH[4].startsWith('newplayer')){window.parent.postMessage("newplayer", "*")} // 嵌入式页面
+            if(INITIAL_PATH[2] != 'live.bilibili.com'){global.rewriteSction();} // 不处理直播页版面
+        }
+        if(INITIAL_PATH[2].match(/live/) == null){
+            document.addEventListener("DOMContentLoaded",global.resetSction()); // 不对直播页进行其他全局处理
+        }
     }
     /* 监听子页面传送的嵌入式播放器信息 */
-    window.addEventListener('message',(event)=>{if(event.data == "newplayer"){console.log("Old Bilibili: Try to change player ",event.data);rewritePage.blackboard();}},false);
+    window.addEventListener('message',(event)=>{if(event.data == "newplayer"){console.log("发现嵌入播放器！开始替换……",event.data + " -> oldplayer");rewritePage.blackboard();}},false);
 })();
