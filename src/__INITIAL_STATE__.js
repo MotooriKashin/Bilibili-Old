151e7dfefe5eac00b70f59322853b9175ca7fce4
MotooriKashin <60416767+MotooriKashin@users.noreply.github.com>
2020-11-06 21:27:44 +0800

    fix syntax error

--- src/__INITIAL_STATE__.js
+++ src/__INITIAL_STATE__.js
@@ -3,20 +3,20 @@
 const BLOD = window.BLOD;
 
 const iniState = {
-    av : (date) => {
+    av: (data) => {
         BLOD.try("__INITIAL_STATE__·av", () => {
-            data = deliver.xhrJsonCheck(data).data;
-                BLOD.aid = BLOD.aid || data.View.aid;
-                BLOD.cid = BLOD.cid || data.View.cid;
-                let dat = {aid : -1, comment : {count : 0, list : []}, error : {}, isClient : false, p: "", player: "", playurl: {}, related : [], tags : [], upData : {}, videoData : {}}
-                dat.aid = data.View.aid;
-                dat.related = data.Related;
-                dat.tags = data.Tags;
-                dat.upData = data.Card.card;
-                dat.upData.archiveCount = data.Card.archive_count;
-                dat.videoData = data.View;
-                dat.videoData.embedPlayer = 'EmbedPlayer("player", "//static.hdslb.com/play.swf", "cid=' + BLOD.cid +'&aid=' + BLOD.aid +'&pre_ad=")'
-                return dat;
+            data = BLOD.jsonCheck(data).data;
+            BLOD.aid = BLOD.aid || data.View.aid;
+            BLOD.cid = BLOD.cid || data.View.cid;
+            let dat = {aid:-1,comment:{count:0,list:[]},error:{},isClient:false,p:"",player:"",playurl:{},related:[],tags:[],upData:{},videoData:{}};
+            dat.aid = data.View.aid;
+            dat.related = data.Related;
+            dat.tags = data.Tags;
+            dat.upData = data.Card.card;
+            dat.upData.archiveCount = data.Card.archive_count;
+            dat.videoData = data.View;
+            dat.videoData.embedPlayer = 'EmbedPlayer("player", "//static.hdslb.com/play.swf", "cid=' + BLOD.cid + '&aid=' + BLOD.aid + '&pre_ad=")';
+            return dat;
         })
     }
 }
