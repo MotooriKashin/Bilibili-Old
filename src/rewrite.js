47e0f83a8f90373838a48b3fdab9eb4888004308
MotooriKashin <60416767+MotooriKashin@users.noreply.github.com>
2020-12-03 17:10:46 +0800

    修复排行榜页面无数据

--- src/rewrite.js
+++ src/rewrite.js
@@ -170,6 +170,7 @@
                 let refer = document.referrer.split("/"), page;
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
                 if (refer && refer[4] && refer[4] == "all") page = BLOD.jsonCheck(BLOD.xhr.false(BLOD.objUrl("https://api.bilibili.com/x/web-interface/ranking", { rid: refer[5], day: 3, type: 1, arc_type: 0 })));
                 else page = BLOD.jsonCheck(BLOD.xhr.false(BLOD.objUrl("https://api.bilibili.com/x/web-interface/ranking", { rid: 0, day: 3, type: 1, arc_type: 0 })));
 >>>>>>> c844375 (fix syntax error)
@@ -181,6 +182,11 @@
                 else page = BLOD.jsonCheck(BLOD.xhr.false(BLOD.objUrl("https://api.bilibili.com/x/web-interface/ranking/v2", { rid: 0, day: 3})));
 >>>>>>> b81f85e (修复排行榜页面无数据)
                 page.data.list.forEach((function(e,i,l) {
+=======
+                if (refer && refer[4] && refer[4] == "all") page = BLOD.jsonCheck(BLOD.xhr.false(BLOD.objUrl("https://api.bilibili.com/x/web-interface/ranking/v2", { rid: refer[5], day: 3 })));
+                else page = BLOD.jsonCheck(BLOD.xhr.false(BLOD.objUrl("https://api.bilibili.com/x/web-interface/ranking/v2", { rid: 0, day: 3 })));
+                page.data.list.forEach((function (e, i, l) {
+>>>>>>> 601d064 (修复排行榜页面无数据)
                     l[i].author = l[i].owner.name;
                     l[i].coins = l[i].stat.coin;
                     l[i].mid = l[i].owner.mid;
