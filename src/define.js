ed4217f887b202d4dd23caad8b954a59c7dacfab
MotooriKashin <60416767+MotooriKashin@users.noreply.github.com>
2020-12-11 16:01:00 +0800

    new secret

--- src/define.js
+++ src/define.js
@@ -132,7 +132,7 @@ const objUrl = (url, obj) => {
         }
     }
 
-    // 生成加密链接
+    // keysecret
     BLOD.urlSign = (url, obj, id) => {
         if (!BLOD.md5) new Function(BLOD.getResourceText("md5"))();
         id = 1 * id || 0;
@@ -145,7 +145,8 @@ const objUrl = (url, obj) => {
             '4_/54d`3_4_73..2c42`d4.a3__31b358d706d`._7a.3_b5.', // Android normal
             '12a.7c4b76c.a`12bb4`2b2b275c667c85b6d`c_c`0d5.051', // BiliLink
             'bb16d652`04.7/121d3474b_2.c12`7386`0/bdd6ca0c7.22', // TV
-            '244_530/7/.ab`7.//22a15572502b_08c21./_.`3164`c36'  // ???
+            '244_530/7/.ab`7.//22a15572502b_08c21./_.`3164`c36',
+            '16_d52_d/d22_2c0a.6573355/b`./bd8a`bc6114a30_4.`d'  // blink
         ][id], key = "";
         obj = typeof obj === "object" ? Object.assign(obj, BLOD.urlObj(url)) : BLOD.urlObj(url);
         url = url.split("?")[0];
