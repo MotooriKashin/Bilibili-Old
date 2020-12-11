440bf271bcb4cbf887321e3f74d9ee947e067d20
MotooriKashin <60416767+MotooriKashin@users.noreply.github.com>
2020-12-11 10:22:52 +0800

    添加视频拦截功能
    拦截播放页视频载入，方便节约流量调用下载功能

--- src/reset.js
+++ src/reset.js
@@ -98,6 +98,7 @@
         // 切P刷新数据
         switchVideo: async () => {
             if (BLOD.avPlus) debug.msg("视频已失效", "缓存信息仅供参考", 300000);
+            if (config.reset.novideo) debug.msg("临时拦截视频载入", "下载完成后务必在设置中关闭！", 300000);
             if (config.reset.download) { BLOD.xml = ""; BLOD.mdf = ""; BLOD.hash = []; };
             if (config.reset.selectdanmu && document.getElementsByClassName("bilibili-player-filter-btn")[1]) document.getElementsByClassName("bilibili-player-filter-btn")[1].click();
             if (config.reset.midcrc && !config.reset.danmuku && !BLOD.hash[0]) xhr.true(BLOD.objUrl("https://api.bilibili.com/x/v1/dm/list.so", { oid: BLOD.cid }));
