ec4f60f91572051412c98f1cdc8916d174333c84
MotooriKashin <60416767+MotooriKashin@users.noreply.github.com>
2020-12-22 20:06:09 +0800

    无区域限制也单解除app限制

--- src/xhrhook.js
+++ src/xhrhook.js
@@ -428,6 +428,7 @@
                     url = BLOD.objUrl(url.split("?")[0], obj);
                     url = url.includes("84956560bc028eb7") ? BLOD.urlSign(url, 0, 8) : url;
                     BLOD.pgc = url.includes("pgc") ? true : false;
+                    if (BLOD.pgc && BLOD.__INITIAL_STATE__.rightsInfo.watch_platform && !BLOD.limit) BLOD.limit = 2;
                     BLOD.vip = BLOD.big > 1 ? true : BLOD.vip;
                     if (BLOD.big > 1 || (BLOD.vip && BLOD.ids.indexOf(1 * BLOD.cid) >= 0)) this.url = url;
                     if (BLOD.limit) this.url = url;
@@ -535,13 +536,16 @@
                                     // 区域限制 + APP限制的DASH似乎缺少码率信息，现默认启用flv以规避，platform用于伪装成APP
                                     if (BLOD.uid && (BLOD.ids.indexOf(1 * BLOD.cid) >= 0) && config.reset.accesskey) accesskey = BLOD.getValue("access_key") || null;
                                     let obj = Object.assign(BLOD.urlObj(this.url), BLOD.__INITIAL_STATE__.rightsInfo.watch_platform ? { access_key: accesskey, fnval: null, fnver: null, module: "pgc", platform: "android_i" } : { access_key: accesskey, module: "pgc" })
-                                    try {
-                                        response = BLOD.jsonCheck(await BLOD.xhr.true(BLOD.objUrl("https://www.biliplus.com/BPplayurl.php", obj)));
-                                    } catch (e) {
-                                        e = Array.isArray(e) ? e : [e];
-                                        debug.error("pgc模式出错", ...e)
-                                        obj.module = "bangumi";
-                                        response = BLOD.jsonCheck(await BLOD.xhr.GM(BLOD.objUrl("https://www.biliplus.com/BPplayurl.php", obj)));
+                                    if (BLOD.limit == 2) response = BLOD.jsonCheck(await BLOD.xhr.GM(BLOD.urlSign("https://api.bilibili.com/pgc/player/api/playurl", Object.assign(obj, { module: null }), 1)));
+                                    else {
+                                        try {
+                                            response = BLOD.jsonCheck(await BLOD.xhr.true(BLOD.objUrl("https://www.biliplus.com/BPplayurl.php", obj)));
+                                        } catch (e) {
+                                            e = Array.isArray(e) ? e : [e];
+                                            debug.error("pgc模式出错", ...e)
+                                            obj.module = "bangumi";
+                                            response = BLOD.jsonCheck(await BLOD.xhr.GM(BLOD.objUrl("https://www.biliplus.com/BPplayurl.php", obj)));
+                                        }
                                     }
                                     response = { "code": 0, "message": "success", "result": response };
                                 }
