f97fda074ff9932e38e96b0dd4078bd59f134cf9
MotooriKashin <60416767+MotooriKashin@users.noreply.github.com>
2021-01-23 08:18:16 +0800

    主动转化av号

--- src/reset.js
+++ src/reset.js
@@ -172,14 +172,15 @@
                     })
                 })
             }
-            BLOD.reset.renameCommentJump();
+            if (config.reset.commentjump) BLOD.reset.renameCommentJump();
         },
         // 重命名评论跳转链接
-        renameCommentJump: async () => {
+        renameCommentJump: () => {
             document.querySelectorAll(".comment-jump-url").forEach((d, i, e) => {
                 if (d.href && !d.href.includes(d.innerText)) {
                     d = d.href.split("/");
                     d = d[d.length - 1] || d[d.length - 2];
+                    if (config.reset.bvid2av && d.toLowerCase().startsWith('bv')) d = BLOD.abv(d);
                     e[i].innerHTML = d;
                 }
             })
