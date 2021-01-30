63d1ff9fc8c5ab424037e6b40c78ab4d8f174e7c
MotooriKashin <60416767+MotooriKashin@users.noreply.github.com>
2021-01-30 15:03:02 +0800

    Update define.js
    尝试同步添加css，不行再异步

--- src/define.js
+++ src/define.js
@@ -159,14 +159,17 @@
             return obj;
         }
         // 添加样式
-        async addCss(css) {
+        addCss(css) {
             if (!css) return;
             let style = document.createElement("style");
             style.setAttribute("type", "text/css");
             style.appendChild(document.createTextNode(css));
-            setTimeout(() => {
-                if (document.head) document.head.appendChild(style)
-            });
+            if (document.head) document.head.appendChild(style);
+            else {
+                setTimeout(() => {
+                    if (document.head) document.head.appendChild(style);
+                })
+            }
         }
         // json校验
         jsonCheck(data) {
