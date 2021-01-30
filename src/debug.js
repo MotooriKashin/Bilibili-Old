477cda64d87ae5765592acea0d54104828c128f5
MotooriKashin <60416767+MotooriKashin@users.noreply.github.com>
2021-01-30 13:34:08 +0800

    Update debug.js
    通知同步到控制台消息

--- src/debug.js
+++ src/debug.js
@@ -2,12 +2,67 @@
  * @module "debug.js"
  * @description 调试模块，以debug对象挂在在BLOD下，基本同console，其中debug.msg发送旧版播放器通知框
  * @method debug/debug.log || debug.error || debug.warn || debug.debug || debug.msg
- * @description2 使用原生JavaScript实现[toastr]的功能
+ * --------------------------------------------------------------------------------
+ * 
+ * @description 使用原生JavaScript实现[toastr]的功能
  * @url https://github.com/CodeSeven/toastr/ MIT license
- * @method2 toast/toast.info || toast.success || toast.warning || toast.error
+ * @method toast/toast.info || toast.success || toast.warning || toast.error
  */
 (function () {
     const BLOD = window.BLOD;
+
+    class Debug {
+        constructor() {
+            console.debug('import module "debug.js"');
+        }
+        log(...msg) {
+            console.log("[" + BLOD.timeFormat(new Date()) + "]", ...msg);
+        }
+        error(...msg) {
+            console.error("[" + BLOD.timeFormat(new Date()) + "]", ...msg);
+        }
+        warn(...msg) {
+            console.warn("[" + BLOD.timeFormat(new Date()) + "]", ...msg);
+        }
+        debug(...msg) {
+            console.debug("[" + BLOD.timeFormat(new Date()) + "]", ...msg);
+        }
+        msg(...msg) {
+            let node = document.getElementsByClassName("bilibili-player-video-toast-bottom")[0];
+            let time = 1 * msg[2] || 3000;
+            if (!node) {
+                this.log(...msg);
+                return;
+            }
+            msg.forEach((d) => { d = typeof d == "object" ? "" : d });
+            msg[2] = 1 * msg[2] ? "" : msg[2];
+            let item = document.createElement("div");
+            node.children[0] ? node.children[0].replaceWith(item) : node.appendChild(item);
+            item.setAttribute("class", "bilibili-player-video-toast-item bilibili-player-video-toast-pay");
+            item.innerHTML = '<div class="bilibili-player-video-toast-item-text"><span class="video-float-hint-text"></span><span class="video-float-hint-btn hint-red"></span><span class="video-float-hint-btn"></span></div>';
+            msg[0] ? item.children[0].children[0].innerHTML = msg[0] : "";
+            msg[1] ? item.children[0].children[1].innerHTML = msg[1] : "";
+            msg[2] ? item.children[0].children[2].innerHTML = msg[2] : item.children[0].children[2].remove();
+            setTimeout(() => item.remove(), time);
+        }
+    }
+
+    const debug = () => {
+        let debug = new Debug();
+        function makeExports(type) {
+            return function (...msg) {
+                return debug[type](...msg);
+            }
+        }
+        let method = makeExports("log");
+        method.log = makeExports("log");
+        method.error = makeExports("error");
+        method.warn = makeExports("warn");
+        method.debug = makeExports("debug");
+        method.msg = makeExports("msg");
+        return method;
+    }
+    BLOD.debug = debug();
 
     // @url https://github.com/CodeSeven/toastr/
     class Toast {
@@ -70,46 +125,20 @@
         }
     }
 
-    class Debug {
-        constructor() {
-            console.debug('import module "debug.js"');
-        }
-        log(...msg) {
-            console.log("[" + BLOD.timeFormat(new Date()) + "]", ...msg);
-        }
-        error(...msg) {
-            console.error("[" + BLOD.timeFormat(new Date()) + "]", ...msg);
-        }
-        warn(...msg) {
-            console.warn("[" + BLOD.timeFormat(new Date()) + "]", ...msg);
-        }
-        debug(...msg) {
-            console.debug("[" + BLOD.timeFormat(new Date()) + "]", ...msg);
-        }
-        msg(...msg) {
-            let node = document.getElementsByClassName("bilibili-player-video-toast-bottom")[0];
-            let time = 1 * msg[2] || 3000;
-            if (!node) {
-                this.log(...msg);
-                return;
-            }
-            msg.forEach((d) => { d = typeof d == "object" ? "" : d });
-            msg[2] = 1 * msg[2] ? "" : msg[2];
-            let item = document.createElement("div");
-            node.children[0] ? node.children[0].replaceWith(item) : node.appendChild(item);
-            item.setAttribute("class", "bilibili-player-video-toast-item bilibili-player-video-toast-pay");
-            item.innerHTML = '<div class="bilibili-player-video-toast-item-text"><span class="video-float-hint-text"></span><span class="video-float-hint-btn hint-red"></span><span class="video-float-hint-btn"></span></div>';
-            msg[0] ? item.children[0].children[0].innerHTML = msg[0] : "";
-            msg[1] ? item.children[0].children[1].innerHTML = msg[1] : "";
-            msg[2] ? item.children[0].children[2].innerHTML = msg[2] : item.children[0].children[2].remove();
-            setTimeout(() => item.remove(), time);
-        }
-    }
-
     const toast = () => {
         let toast = new Toast();
         function makeExports(type) {
             return function (...msg) {
+                switch (type) {
+                    case "info": BLOD.debug.debug(...msg);
+                        break;
+                    case "error": BLOD.debug.error(...msg);
+                        break;
+                    case "success": BLOD.debug(...msg);
+                        break;
+                    case "warning": BLOD.debug.warn(...msg);
+                        break;
+                }
                 return toast.show(type, ...msg);
             }
         }
@@ -120,24 +149,6 @@
         method.warning = makeExports("warning");
         return method;
     }
-
-    const debug = () => {
-        let debug = new Debug();
-        function makeExports(type) {
-            return function (...msg) {
-                return debug[type](...msg);
-            }
-        }
-        let method = makeExports("log");
-        method.log = makeExports("log");
-        method.error = makeExports("error");
-        method.warn = makeExports("warn");
-        method.debug = makeExports("debug");
-        method.msg = makeExports("msg");
-        return method;
-    }
-
     BLOD.toast = toast();
-    BLOD.debug = debug();
 
 })()
