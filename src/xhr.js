04939ab8e08ceeef9c254ccaec03598010205f4d
MotooriKashin <60416767+MotooriKashin@users.noreply.github.com>
2021-02-17 18:06:42 +0800

    完善xhr封装

--- src/xhr.js
+++ src/xhr.js
@@ -12,19 +12,46 @@
             BLOD.GMxhrLog = [];
             console.debug('import module "xhr.js"');
         }
+<<<<<<< HEAD
         false(url) {
+=======
+        /**
+         * 同步链接
+         * @param {string} url 链接url
+         * @param {boolean} [credentials] 设定是否携带cookies，默认为 true
+         */
+        false(url, credentials = true) {
+>>>>>>> 8699635 (完善xhr封装)
             const xhr = new XMLHttpRequest();
             xhr.open('GET', url, false);
-            xhr.withCredentials = true;
+            xhr.withCredentials = credentials;
             xhr.send(null);
             return xhr.responseText;
         }
+<<<<<<< HEAD
         true(url) {
             return new Promise((resolve, reject) => {
                 let xhr = new XMLHttpRequest();
                 xhr.open('get', url, true);
                 xhr.withCredentials = true;
                 xhr.onload = () => resolve(xhr.responseText);
+=======
+        /**
+         * 异步链接
+         * @param {string} url 链接url
+         * @param {string} [responseType] 设定服务器返回值
+         * @param {object} [headers] 设定请求头键值对，注意有些属性是不可修改的
+         * @param {boolean} [credentials] 设定是否携带cookies，默认为 true
+         */
+        true(url, responseType = "text", headers = {}, credentials = true) {
+            return new Promise((resolve, reject) => {
+                let xhr = new XMLHttpRequest();
+                xhr.open('get', url, true);
+                xhr.responseType = responseType;
+                for (let key in headers) if (key && headers[key]) xhr.setRequestHeader(key, headers[key]);
+                xhr.withCredentials = credentials;
+                xhr.onload = () => resolve(xhr.response);
+>>>>>>> 8699635 (完善xhr封装)
                 xhr.onerror = () => {
                     toast.error("XMLHttpRequest 错误！", "method：GET", "url：" + url, xhr.statusText || "net::ERR_CONNECTION_TIMED_OUT");
                     reject(xhr.statusText || url + " net::ERR_CONNECTION_TIMED_OUT");
@@ -32,11 +59,21 @@
                 xhr.send();
             });
         }
+<<<<<<< HEAD
         GM(url) {
+=======
+        /**
+         * 跨域链接
+         * @param {string} url 链接url
+         * @param {object} [headers] 设定请求头：user-agent, referer, ...
+         */
+        GM(url, headers = {}) {
+>>>>>>> 8699635 (完善xhr封装)
             return new Promise((resolve, reject) => {
                 BLOD.xmlhttpRequest({
                     method: "GET",
                     url: url,
+                    headers: headers,
                     onload: (xhr) => {
                         BLOD.GMxhrLog.push([BLOD.timeFormat(new Date()), url, (String(xhr.responseText).startsWith("{") ? JSON.parse(xhr.responseText) : xhr.responseText)]);
                         resolve(xhr.responseText);
@@ -48,13 +85,23 @@
                 });
             })
         }
+<<<<<<< HEAD
         post(url, header, data) {
+=======
+        /**
+         * post方法
+         * @param {string} url 链接url
+         * @param {object} [headers] 设定请求头，注意有些属性是不可修改的
+         * @param {*} [data] 所需提交的数据，post方法专属
+         * @param {boolean} [credentials] 设定是否携带cookies，默认为 true
+         */
+        post(url, headers = { "Content-type": "application/x-www-form-urlencoded" }, data, credentials = true) {
+>>>>>>> 8699635 (完善xhr封装)
             return new Promise((resolve, reject) => {
                 let xhr = new XMLHttpRequest();
-                header = header || "application/x-www-form-urlencoded";
                 xhr.open('post', url, true);
-                xhr.setRequestHeader("Content-type", header);
-                xhr.withCredentials = true;
+                for (let key in headers) if (key && headers[key]) xhr.setRequestHeader(key, headers[key]);
+                xhr.withCredentials = credentials;
                 xhr.onload = () => resolve(xhr.responseText);
                 xhr.onerror = () => {
                     toast.error("XMLHttpRequest 错误！", "method：POST", "url：" + url, xhr.statusText || "net::ERR_CONNECTION_TIMED_OUT");
