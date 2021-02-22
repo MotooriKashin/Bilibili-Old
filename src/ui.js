78dfb5cf034dfdd0d85be51ed7bfc7e7712a2aec
MotooriKashin <60416767+MotooriKashin@users.noreply.github.com>
2021-02-22 08:34:19 +0800

    Typo fix

--- src/ui.js
+++ src/ui.js
@@ -519,7 +519,7 @@
                     if (!/^\d+$/.test(step.value) || step.value > 1000 || step.value < 50) return;
                     BLOD.toast.config.step = step.value;
                     BLOD.toast.change(BLOD.toast.config);
-                    toast.success("调整通知延时：" + step.value + " 豪秒");
+                    toast.success("调整通知延时：" + step.value + " 毫秒");
                 }
                 let button = BLOD.addElement("div", {}, right);
 >>>>>>> eea4f89 (重绘设置界面)
