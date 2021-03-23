c8978c79d483e5e5a26e0856eeb21fe76631c065
MotooriKashin <60416767+MotooriKashin@users.noreply.github.com>
2021-03-23 10:57:20 +0800

    弹幕排序

--- src/xhrhook.js
+++ src/xhrhook.js
@@ -415,7 +415,7 @@
      */
     const toXml = BLOD.toXml = (danmaku) => {
         return new Promise(function (resolve) {
-            danmaku.sort((a, b) => (BLOD.bigInt(a.idStr, b.idStr) ? 1 : -1));
+            danmaku.sort((a, b) => (BLOD.bigInt(a.idStr, b.idStr) ? -1 : 1));
             let attr = [], xml = '<?xml version="1.0" encoding="UTF-8"?><i><chatserver>chat.bilibili.com</chatserver><chatid>' + BLOD.cid + '</chatid><mission>0</mission><maxlimit>99999</maxlimit><state>0</state><real_name>0</real_name><source>e-r</source>'
             for (let i = 0; i < danmaku.length; i++) {
                 attr[0] = danmaku[i].progress / 1000;
@@ -515,7 +515,7 @@
             };
         }
         specialEffects(danmaku);
-        danmaku.sort((a, b) => (BLOD.bigInt(a.dmid, b.dmid) ? 1 : -1));
+        danmaku.sort((a, b) => (BLOD.bigInt(a.dmid, b.dmid) ? -1 : 1));
         /**
          * bilibiliPlayer.js 21394行已经添加如下代码，用于设置弹幕池
          * @param  {Array} dm 弹幕数组
@@ -793,7 +793,7 @@
                         if (BLOD.aid < 400000) {
                             specialEffects(danmaku);
                         }
-                        danmaku.sort((a, b) => (BLOD.bigInt(a.dmid, b.dmid) ? 1 : -1));
+                        danmaku.sort((a, b) => (BLOD.bigInt(a.dmid, b.dmid) ? -1 : 1));
                         parseTime = new Date() - parseTime;
                         list_so.onmessage({
                             data: {
