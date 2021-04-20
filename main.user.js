284bb75dde1e04ff864a090e059f7becf4d94905
MotooriKashin <60416767+MotooriKashin@users.noreply.github.com>
2021-04-20 20:24:19 +0800

    精简合并基础模块

--- main.user.js
+++ main.user.js
@@ -26,8 +26,6 @@
 // @resource     md5 https://cdn.jsdelivr.net/gh/MotooriKashin/Bilibili-Old/src/md5.js
 // @resource     iniState https://cdn.jsdelivr.net/gh/MotooriKashin/Bilibili-Old/src/initialstate.js
 // @resource     ui https://cdn.jsdelivr.net/gh/MotooriKashin/Bilibili-Old@05a493119f139c1f6ac0dd52dd2089442d8a0022/src/ui.js
-// @resource     debug https://cdn.jsdelivr.net/gh/MotooriKashin/Bilibili-Old@4bca5f5184c7f3368f46ebb613b94580febc8d8b/src/debug.js
-// @resource     xhr https://cdn.jsdelivr.net/gh/MotooriKashin/Bilibili-Old/src/xhr.js
 // @resource     download https://cdn.jsdelivr.net/gh/MotooriKashin/Bilibili-Old@c67c01a0460dddfa48abb29efa6415c2ebaf1a81/src/download.js
 // @resource     define https://cdn.jsdelivr.net/gh/MotooriKashin/Bilibili-Old@6c8defcbc30c9e0cda074fc7fd811b6f00b98548/src/define.js
 // @resource     rewrite https://cdn.jsdelivr.net/gh/MotooriKashin/Bilibili-Old@48b39f9a2762d9ea8a2102166003ddc20a5142d0/src/rewrite.js
@@ -51,14 +49,10 @@
 // ==/UserScript==
 
 (function () {
-    'use strict';
-
     // 获取默认设置
     const config = JSON.parse(GM_getResourceText("config"));
     // 暴露protobuf接口
     unsafeWindow.protobuf = window.protobuf;
-
-    let aid, cid, bvid;
 
     // 暴露顶层接口
     const BLOD = unsafeWindow.BLOD = {
@@ -68,124 +62,104 @@
         getResourceText: GM_getResourceText,
         getResourceURL: GM_getResourceURL,
         deleteValue: GM_deleteValue,
-        aid: aid,
-        cid: cid,
-        bvid: bvid,
         hash: [],
         ids: [],
         bloburl: {},
+        module: [],
         title: document.title.includes("出错") ? null : document.title
     }
+    /**
+     * 获取全cookies
+     * @returns {Promise<{}>} 完全的cookies对象
+     */
+    BLOD.cookie = () => {
+        return new Promise((resolve, reject) => {
+            GM_cookie.list({}, e => {
+                let obj = {};
+                e.forEach(d => {
+                    obj[d.name] = d.value;
+                });
+                resolve(obj);
+            });
+        })
+    }
+    /**
+     * 导入模块
+     * @param {string} moduleName 模块名字，由`@resource`源数据定义
+     * @returns {void} void
+     */
+    BLOD.importModule = (moduleName) => {
+        try {
+            if (BLOD.module.includes(moduleName)) return;
+            new Function(BLOD.getResourceText(moduleName))();
+            BLOD.module.push(moduleName);
+            if (BLOD.debug) BLOD.debug.info("载入模块", moduleName);
+            else console.info("载入模块", moduleName);
+        } catch (e) {
+            if (BLOD.toast) BLOD.toast.error(`载入 ${moduleName} 模块失败！`, e);
+            else console.error(`载入 ${moduleName} 模块失败！`, e);
+        }
+    }
     // 导入全局模块，其他模块按需加载
-    new Function(GM_getResourceText("define"))();
-    new Function(GM_getResourceText("debug"))();
-    new Function(GM_getResourceText("xhr"))();
-    new Function(GM_getResourceText("iniState"))();
-    const debug = BLOD.debug;
-    const xhr = BLOD.xhr;
-
-    // 修复退出登录功能
-    if (location.href.includes("bilibili.com/login?act=exit")) {
-        (async () => {
-            let refer = document.referrer;
-            await xhr.post("https://passport.bilibili.com/login/exit/v2", "biliCSRF=" + BLOD.getCookies().bili_jct + "&gourl=" + encodeURIComponent(location.href));
-            location.href = refer;
-        })();
-    }
-    // 初始化配置数据
-    let localConfig = BLOD.getValue("config");
-    let configSort = ["rewrite", "reset"];
-    BLOD.defaultConfig = JSON.parse(JSON.stringify(config));
-    for (let key in config) if (configSort.indexOf(key) < 0) delete config[key];
-    if (localConfig) {
-        configSort.forEach(x => {
-            for (let key in localConfig[x]) if (key in config[x]) config[x][key] = localConfig[x][key];
-            for (let key in config[x]) config[x][key] = Array.isArray(config[x][key]) ? config[x][key][0] : config[x][key];
-        })
-    } else {
-        configSort.forEach(x => {
-            for (let key in config[x]) config[x][key] = config[x][key][0];
-        })
-        BLOD.setValue("config", config);
-    }
-    BLOD.config = config;
-    new Function(GM_getResourceText("reset"))();
+    BLOD.importModule("define"); // 基础模块
+    BLOD.importModule("reset"); // 修改模块
+    BLOD.importModule("rewrite"); // 重写模块
     // 处理参数及BV号
     BLOD.reset.parameterTrim();
-    BLOD.uid = BLOD.getCookies().DedeUserID;
-    BLOD.path = document.location.href.split('/');
-    // 捕获window属性
-    BLOD.reset.getVariable();
-    if (BLOD.uid) {
-        let offset = BLOD.getCookies()["bp_video_offset_" + BLOD.uid];
-        if (offset) document.cookie = "bp_t_offset_" + BLOD.uid + "=" + offset + "; domain=bilibili.com; expires=Aug, 18 Dec 2038 18:00:00 GMT; BLOD.path=/";
-    }
     // 页面分离
     if (BLOD.path[3]) {
         if (BLOD.path[3] == 'video' && (BLOD.path[4].toLowerCase().startsWith('av') || BLOD.path[4].toLowerCase().startsWith('bv'))) {
-            if (!BLOD.rewrite) new Function(GM_getResourceText("rewrite"))();
             BLOD.rewrite.av();
         }
         if (BLOD.path[3] == 'watchlater') {
-            if (!BLOD.rewrite) new Function(GM_getResourceText("rewrite"))();
             BLOD.rewrite.watchlater();
         }
         if (BLOD.path[3] == 'bangumi' && BLOD.path[4] == 'play') {
-            if (!BLOD.rewrite) new Function(GM_getResourceText("rewrite"))();
             BLOD.rewrite.bangumi();
         }
         if (BLOD.path[3] == 'blackboard' && BLOD.path[4]) {
-            if (!BLOD.rewrite) new Function(GM_getResourceText("rewrite"))();
             BLOD.rewrite.blackboard();
         }
         if (BLOD.path[3] == 'playlist' && BLOD.path[5].startsWith('pl')) {
-            if (!BLOD.rewrite) new Function(GM_getResourceText("rewrite"))();
             BLOD.rewrite.playlist();
         }
         if (BLOD.path[3] == 'medialist' && BLOD.path[4] == 'play') {
-            if (!BLOD.rewrite) new Function(GM_getResourceText("rewrite"))();
             BLOD.rewrite.medialist();
         }
         if (BLOD.path[3] == 'festival') {
-            if (!BLOD.rewrite) new Function(GM_getResourceText("rewrite"))();
             BLOD.rewrite.festival();
         }
         if (BLOD.path[3] == 's' && (BLOD.path[5].toLowerCase().startsWith('av') || BLOD.path[5].toLowerCase().startsWith('bv'))) {
-            if (!BLOD.rewrite) new Function(GM_getResourceText("rewrite"))();
             BLOD.rewrite.s();
         }
         if (BLOD.path[2] == 'space.bilibili.com') {
-            if (!BLOD.rewrite) new Function(GM_getResourceText("rewrite"))();
-            BLOD.rewrite.space();
+            if (!BLOD.rewrite) BLOD.rewrite.space();
         }
         if (BLOD.path[2] == 'www.bilibili.com' && (BLOD.path[3].startsWith('\?') || BLOD.path[3].startsWith('\#') || BLOD.path[3].startsWith('index.'))) {
-            if (!BLOD.rewrite) new Function(GM_getResourceText("rewrite"))();
             BLOD.rewrite.index();
         }
         if (BLOD.path[3] == 'v' && BLOD.path[4] == "popular") {
-            if (!BLOD.rewrite) new Function(GM_getResourceText("rewrite"))();
             BLOD.rewrite.rank();
         }
         if (BLOD.path[2] == 'live.bilibili.com') {
             BLOD.path.name = "live";
+            if (BLOD.config.reset.roomplay) BLOD.getVariable(window, "__NEPTUNE_IS_MY_WAIFU__", undefined, [undefined]);
             BLOD.reset.fuckp2p();
             BLOD.reset.disableLiveSleep();
         }
     } else {
         if (BLOD.path[2] == 'www.bilibili.com') {
-            if (!BLOD.rewrite) new Function(GM_getResourceText("rewrite"))();
             BLOD.rewrite.index();
         }
     }
 
     // 处理消息页面样式
     if (BLOD.path[2] == "message.bilibili.com") BLOD.addCss(".container[data-v-6969394c] { height: calc(100vh - 42px) !important;} .container[data-v-1c9150a9] { height: calc(100vh - 42px) !important;}");
-
     if (window.self == window.top && BLOD.path[2] == 'www.bilibili.com') document.domain = "bilibili.com";
     // 写入全局样式
     BLOD.addCss(BLOD.getResourceText("css"));
-    new Function(GM_getResourceText("ui"))();
-    new Function(GM_getResourceText("xhrhook"))();
+    BLOD.importModule("ui");
+    BLOD.importModule("xhrhook");
     window.addEventListener("load", () => { BLOD.load = true; BLOD.reset.parameterTrim(true); });
     document.addEventListener("DOMNodeInserted", (msg) => {
         // 去除预览提示框
@@ -196,7 +170,7 @@
         if (/bilibili-player-video-btn-start/.test(msg.target.className)) BLOD.reset.switchVideo();
         // 创建播放器右键下载菜单
         if (/bilibili-player-context-menu-container black/.test(msg.target.className)) {
-            if (!BLOD.download) new Function(GM_getResourceText("download"))();
+            if (!BLOD.download) BLOD.importModule("download");
             BLOD.download.init(msg.target);
         }
         // 修复失效频道视频
