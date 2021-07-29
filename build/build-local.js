32d5cac1dd0862018e671a3c03ec0ad84de6bdfd
MotooriKashin <60416767+MotooriKashin@users.noreply.github.com>
2021-07-29 14:44:03 +0800

    添加任务：生成本地/在线版本

---
+++ build/build-local.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const meta = require("../meta.json");
+const resource = require("../resource.json")
+
+fs.readFile("./JavaScript/index.js", "utf-8", (err, data) => {
+    if (err) throw err;
+    let content = Object.keys(meta).reduce((s, d) => {
+        s = Array.isArray(meta[d]) ? meta[d].reduce((a, b) => {
+            a = `${a}// @${d.padEnd(13, " ")}${b}\r\n`;
+            return a;
+        }, s) : `${s}// @${d.padEnd(13, " ")}${meta[d]}\r\n`;
+        return s;
+    }, "// ==UserScript==\r\n");
+    content = Object.keys(resource).reduce((s, d) => {
+        switch (d.split(".")[1]) {
+            case "js": s = `${s}// @resource     ${d} file:///${process.argv[2].replace(/\\/g, "/")}/JavaScript/${d}\r\n`;
+                break;
+            case "json": s = `${s}// @resource     ${d} file:///${process.argv[2].replace(/\\/g, "/")}/Json/${d}\r\n`;
+                break;
+            case "html": s = `${s}// @resource     ${d} file:///${process.argv[2].replace(/\\/g, "/")}/HTML/${d}\r\n`
+        }
+        return s;
+    }, content) + "// ==/UserScript==\r\n" + data.replace("\"use strict\";", "");
+    fs.writeFile("./local.user.js", content, (err) => { if (err) throw err })
+})
