06f23d23ee183a97fa086d35a99cca7f544a67c0
MotooriKashin <60416767+MotooriKashin@users.noreply.github.com>
2021-09-13 19:48:57 +0800

    bannerGif: 临时修复banner
    旧版顶栏banner已失效，移植中~

--- module/required/bannerGif.ts
+++ module/required/bannerGif.ts
@@ -13,4 +13,18 @@
             }
         }
     })
+    API.jsonphook(["api.bilibili.com/x/web-show/res/locs", "ids=142"], function (xhr) {
+        const obj = API.urlObj(xhr.url);
+        let callback: any = obj.callback;
+        let call: any = window[callback];
+        if (call) {
+            (<any>window)[callback] = function (v: any) {
+                v.data[142][0].pic = API.randomArray([
+                    "//i0.hdslb.com/bfs/activity-plat/static/20171220/68a052f664e8414bb594f9b00b176599/images/90w1lpp6ry.png",
+                    "//i0.hdslb.com/bfs/archive/b60151347754f0089489d7d68271233960a8d52f.png"
+                ], 1)[0];
+                return call(v);
+            }
+        }
+    })
 })();
