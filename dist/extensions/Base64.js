82a13f5b697393534818a8818d1db25f5b3628f1
MotooriKashin <60416767+MotooriKashin@users.noreply.github.com>
2021-09-19 09:11:54 +0800

    Base64.ts: 添加Base64编解码工具

---
+++ dist/extensions/Base64.js
@@ -0,0 +1,24 @@
+/**
+ * 本模块负责提供Base64<=>字符串的互转函数
+ */
+try {
+    class Base64 {
+        static encode(str) {
+            return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function (match, p1) {
+                return String.fromCharCode(('0x' + p1));
+            }));
+        }
+        static decode(str) {
+            return decodeURIComponent(atob(str).split('').map(function (c) {
+                return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+            }).join(''));
+        }
+    }
+    API.Base64 = {
+        encode: (str) => Base64.encode(str),
+        decode: (str) => Base64.decode(str)
+    };
+}
+catch (e) {
+    API.trace(e, "base64.js", true);
+}
