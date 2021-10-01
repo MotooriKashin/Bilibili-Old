3135f4c584e5670c1dcee1fb04233101359080af
MotooriKashin <60416767+MotooriKashin@users.noreply.github.com>
2021-10-01 19:27:46 +0800

    修复视频简介中BV号超链接 #176

--- JavaScript/rewrite.js
+++ JavaScript/rewrite.js
@@ -1049,12 +1049,11 @@
             let desc = document.getElementsByClassName("info");
             if (desc[1] && desc[1].parentNode && desc[1].parentNode.id == "v_desc") {
                 if (desc[1].outerHTML.match(/BV[A-Za-z0-9]+/i)) {
-                    let paster = desc[1].outerHTML.match(/BV[A-Za-z0-9]+/i);
-                    for (let i = 0; i < paster.length; i++) {
-                        let newer = "av" + BLOD.abv(paster[i]);
-                        newer = '<a target="_blank" href="//www.bilibili.com/video/' + newer + '">' + newer + '</a>';
-                        desc[1].innerHTML = desc[1].outerHTML.replace(paster[i], newer);
-                    }
+                    const text = desc[1].innerText.replace(/BV[A-Za-z0-9]+/gi, (str) => {
+                        const av = API.abv(str);
+                        return `<a target="_blank" href="//www.bilibili.com/video/av${av}">av${av}</a>`;
+                    });
+                    desc[1].innerHTML = text;
                 }
             }
         }
