47fce4a4b4790cb791b91be4797671956de798bb
MotooriKashin <60416767+MotooriKashin@users.noreply.github.com>
2021-11-27 16:20:39 +0800

    修复空间顶栏样式错误

--- src/required/section.ts
+++ src/required/section.ts
@@ -5,6 +5,7 @@
     try {
         API.runWhile(() => document.querySelector("#internationalHeader"), () => {
             if (API.path.name) return;
+            API.addCss(".nav-item.live {width: auto;}");
             (<HTMLDivElement>document.querySelector("#internationalHeader")).setAttribute("style", "visibility:hidden;");
             (!(<any>window).$?.ajax) && API.addElement("script", { type: "text/javascript", src: "//static.hdslb.com/js/jquery.min.js" }, undefined, undefined, true);
             (document.querySelector(".mini-type") && !location.href.includes("blackboard/topic_list") && !location.href.includes("blackboard/x/act_list")) ? API.addElement("div", { class: "z-top-container" }, undefined, undefined, true) : API.addElement("div", { class: "z-top-container has-menu" }, undefined, undefined, true);
