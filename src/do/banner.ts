b4e73a651dcda461c61eb7add7f251f7d6486dfb
MotooriKashin <60416767+MotooriKashin@users.noreply.github.com>
2022-02-21 16:52:14 +0800

    修复顶栏广场菜单

--- src/do/banner.ts
+++ src/do/banner.ts
@@ -367,4 +367,7 @@ interface modules {
         setTimeout(() => new Animate(header.data));
         return loc;
     }, false);
+    API.jsonphookasync("api.bilibili.com/plaza/banner", () => true, async url => {
+        return { "code": 0, "result": [{ "link": "https://www.bilibili.com/blackboard/x/act_list", "end": 1640966407, "begin": 1456709887, "title": "bilibili 活动", "cover": "http://i0.hdslb.com/bfs/square/6830d0e479eee8cc9a42c3e375ca99a5147390cd.jpg", "id": 9, "created_ts": 1491386053 }, { "link": "http://www.bilibili.com/blackboard/topic_list.html", "end": 1640966418, "begin": 1544258598, "title": "话题列表", "cover": "http://i0.hdslb.com/bfs/square/b1b00a0c3ce8570b48277ae07a2e55603a4a4ddf.jpg", "id": 17, "created_ts": 1491386030 }] }
+    }, false);
 }
