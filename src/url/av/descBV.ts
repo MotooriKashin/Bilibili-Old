4e9031da93b0523b51d125a90393c8001de92fe6
MotooriKashin <60416767+MotooriKashin@users.noreply.github.com>
2022-03-09 07:21:24 +0800

    acg短域名失效替换为直链

--- src/url/av/descBV.ts
+++ src/url/av/descBV.ts
@@ -8,11 +8,17 @@ namespace API {
             let desc = document.getElementsByClassName("info");
             if (desc[1] && desc[1].parentNode && (<HTMLElement>desc[1].parentNode).id == "v_desc") {
                 let text: any = (<HTMLDivElement>desc[1]).textContent;
-                text = text.replace(/[bB][vV]1[fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF]{9}/g, (str: any) => {
-                    return "av" + API.abv(str);;
-                }).replace(/(av|sm)[0-9]+/gi, (str: any) => {
+                text = text.replace(/[bB][vV]1[fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF]{9}/g, (str: string) => {
+                    return "av" + abv(str);;
+                }).replace(/av[0-9]+/gi, (str: string) => {
                     str = str.toLowerCase();
-                    return `<a target="_blank" href="http://acg.tv/${str}">${str}</a>`; // 此域名并未升级https！
+                    return `<a target="_blank" href="//www.bilibili.com/video/${str}">${str}</a>`;
+                }).replace(/sm[0-9]+/gi, (str: string) => {
+                    str = str.toLowerCase();
+                    return `<a target="_blank" href="//www.nicovideo.jp/watch/${str}">${str}</a>`;
+                }).replace(/cv[0-9]+/gi, (str: string) => {
+                    str = str.toLowerCase();
+                    return `<a target="_blank" href="//www.bilibili.com/read/${str}">${str}</a>`;
                 });
                 desc[1].innerHTML = text;
             }
