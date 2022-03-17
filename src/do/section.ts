24f63b9968750870c8e1fde5c897086c4635c072
MotooriKashin <60416767+MotooriKashin@users.noreply.github.com>
2022-03-17 10:56:09 +0800

    #268 重构顶栏-V3

--- src/do/section.ts
+++ src/do/section.ts
@@ -4,6 +4,7 @@ interface modules {
     readonly "avatarAnimation.css": string;
 }
 namespace API {
+    // 替换顶栏
     runWhile(() => document.querySelector("#internationalHeader"), () => {
         if (path.name) return;
         addCss(".nav-item.live {width: auto;}");
@@ -17,6 +18,7 @@ namespace API {
             addElement("div", { class: "z-top-container has-menu" }, undefined, undefined, true);
         addElement("script", { type: "text/javascript", src: "//s1.hdslb.com/bfs/seed/jinkela/header/header.js" });
     })
+    // 替换底栏
     runWhile(() => document.querySelector(".international-footer"), () => {
         if (path.name) return;
         (<HTMLDivElement>document.querySelector(".international-footer")).remove();
@@ -24,8 +26,15 @@ namespace API {
         addElement("div", { class: "footer bili-footer report-wrap-module", id: "home_footer" });
         addElement("script", { type: "text/javascript", src: "//static.hdslb.com/common/js/footer.js" });
     })
+    // 移除新版顶栏
     runWhile(() => document.querySelector("#bili-header-m"), () => {
         (<HTMLDivElement>document.querySelector("#internationalHeader"))?.remove();
         addCss(getModule("avatarAnimation.css"));
+    })
+    // 替换第三版顶栏
+    runWhile(() => document.body.classList.contains("header-v3"), () => {
+        if (path.name) return;
+        document.body.classList.remove("header-v3");
+        loadScript("//s1.hdslb.com/bfs/seed/jinkela/header/header.js")//, () => document.body.classList.remove("header-v3"));
     })
 }
