731537b38f69ac4716765bc4936ec850a641eea9
MotooriKashin <60416767+MotooriKashin@users.noreply.github.com>
2022-07-10 09:16:41 +0800

    去除评论区放大镜

--- src/bilibili/comment.js
+++ src/bilibili/comment.js
@@ -1093,8 +1093,8 @@
                     var r,
                         i,
                         a,
-                        o;
-                    0 <= e.indexOf(t) && (r = (o = n[t]).prefix_icon ? '<img src="' + o.prefix_icon + '" class="jump-img"/>' : "", a = f.unhtml(o.title), i = o.pc_url ? o.pc_url : 0 === t.indexOf("http") ? t : this._createLinkById(t), a = r + (0 === o.state ? '<a href="' + i + '" data-report="' + this.jumpReportIndex + '" class="comment-jump-url" target="_blank">' + a + "</a>" : a), this.jumpReport[this.jumpReportIndex] = o.click_report, this.jumpReportIndex++, o = new RegExp(t.replace(/\?/, "\\?"), "ig"), e = e.replace(o, a))
+                        o = n[t];
+                    0 <= e.indexOf(t) && !o.pc_url && (r = o.prefix_icon ? '<img src="' + o.prefix_icon + '" class="jump-img"/>' : "", a = f.unhtml(o.title), i = 0 === t.indexOf("http") ? t : this._createLinkById(t), a = r + (0 === o.state ? '<a href="' + i + '" data-report="' + this.jumpReportIndex + '" class="comment-jump-url" target="_blank">' + a + "</a>" : a), this.jumpReport[this.jumpReportIndex] = o.click_report, this.jumpReportIndex++, o = new RegExp(t.replace(/\?/, "\\?"), "ig"), e = e.replace(o, a))
                 }
                 return e
             },
