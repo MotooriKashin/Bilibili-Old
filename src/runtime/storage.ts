daea6ce61921945536356a99225eb47b7ce522bd
MotooriKashin <60416767+MotooriKashin@users.noreply.github.com>
2022-07-22 12:59:04 +0800

    不使用私有属性

--- src/runtime/storage.ts
+++ src/runtime/storage.ts
@@ -1,18 +1,18 @@
 class StorageInterface {
     [name: string]: any; // 允许任意字符串索引
-    #_: boolean; // 类型标记：localStorage/sessionStorage
+    _: boolean; // 类型标记：localStorage/sessionStorage
     /**
      * 
      * @param target 类型标记：真 ? sessionStorage : localStorage
      */
     constructor(target: boolean) {
-        this.#_ = target;
+        this._ = target;
         // 原生Storage支持以属性形式获取/修改存储，使用get/set模拟。
         this.keys().forEach(d => Object.defineProperty(this, d, { get: () => this.getItem(d), set: v => this.setItem(d, v) }));
     }
     /** 清空！ */
     clear() {
-        (this.#_ ? self.sessionStorage : self.localStorage).clear()
+        (this._ ? self.sessionStorage : self.localStorage).clear()
     };
     /**
      * 读取
@@ -20,7 +20,7 @@ class StorageInterface {
      * @returns 格式化后的数据
      */
     getItem(key: string) {
-        let str: any = (this.#_ ? self.sessionStorage : self.localStorage).getItem(key);
+        let str: any = (this._ ? self.sessionStorage : self.localStorage).getItem(key);
         try { str = JSON.parse(str) } catch (e) { }
         return str;
     };
@@ -30,14 +30,14 @@ class StorageInterface {
      * @returns 键名数组
      */
     keys() {
-        return Object.keys((this.#_ ? self.sessionStorage : self.localStorage));
+        return Object.keys((this._ ? self.sessionStorage : self.localStorage));
     };
     /**
      * 移除
      * @param key 目标键名
      */
     removeItem(key: string) {
-        (this.#_ ? self.sessionStorage : self.localStorage).removeItem(key);
+        (this._ ? self.sessionStorage : self.localStorage).removeItem(key);
     };
     /**
      * 添加/修改
@@ -46,15 +46,15 @@ class StorageInterface {
      */
     setItem(key: string, value: any) {
         switch (typeof value) {
-            case "object": (this.#_ ? self.sessionStorage : self.localStorage).setItem(key, JSON.stringify(value));
+            case "object": (this._ ? self.sessionStorage : self.localStorage).setItem(key, JSON.stringify(value));
                 break;
             case "function": console.warn("函数类型并不适合这样存储！", key, value);
                 break;
-            default: (this.#_ ? self.sessionStorage : self.localStorage).setItem(key, String(value));
+            default: (this._ ? self.sessionStorage : self.localStorage).setItem(key, String(value));
         }
     };
     /** 条目总数 */
-    get length() { return (this.#_ ? self.sessionStorage : self.localStorage).length }
+    get length() { return (this._ ? self.sessionStorage : self.localStorage).length }
 }
 /** localStorage */
 class LocalStorage extends StorageInterface {
